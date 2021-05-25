4d50428bc61c1e919abfe0a0d6ffe158f43373ea
crraxx <78258888+crraxx@users.noreply.github.com>
2021-05-25 14:30:02 +0800

    Add files via upload

---
+++ EveryDay/PalindromeLinkedList234.java
@@ -0,0 +1,34 @@
+package EveryDay;
+
+import java.util.ArrayList;
+
+public class PalindromeLinkedList234 {
+    /*
+    请判断一个链表是否为回文链表。
+        示例 1:
+        输入: 1->2
+        输出: false
+        示例 2:
+        输入: 1->2->2->1
+        输出: true
+     */
+
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<ListNode> list = new ArrayList<ListNode>();
+        while(head != null){
+            list.add(head);
+            head = head.next;
+        }
+
+        int left = 0;
+        int right = list.size()-1;
+        while(left < right){
+            if (!list.get(left).val == list.get(right).val){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
