adc8d03dcedf8aaeb870f26aa079385affbe98f8
crraxx <78258888+crraxx@users.noreply.github.com>
2021-05-29 11:43:05 +0800

    Add files via upload

---
+++ NiuKe/从尾到头打印链表.java
@@ -0,0 +1,20 @@
+package NiuKe;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class 从尾到头打印链表 {
+
+   /* public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        while (stack.size()!=0) {
+            list.add(stack.pop());
+        }
+        return list;
+    }*/
+}
